6ea6dc865c72ad6ba54fa4ee05c0566d12cdac5a
EslamMagdiFcis <eslam.magdi.fcis@gmail.com>
2019-08-25 23:50:36 +0200

    Added LogOut View

---
+++ src/components/logOut.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data(){
+        return {
+        }
+    },
+    
+    created() {
+      this.logOut()  
+    },
+    
+    methods:{
+        logOut(){
+          localStorage.removeItem('user');
+          this.$router.push({name:'login'});
+        },
+    }
+  }
+</script>
